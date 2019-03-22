1e5f6f346ee06b7cbfa71ccd0d39922d2c3285b8
aniketQED42 <aniket.gaikwad@QED42.com>
2019-03-22 14:03:10 +0530

    Worked on twig variables and adding classes

---
+++ sites/default/files/php/twig/5c949360bfe3b_node.html.twig_xFUeh3Pn33gA_Kn7ssAo4Z5Zi/VVO0eXZXFy4-L4VYTC9f6hpGpsmpuHVVYaVhWcUnIfw.php
@@ -0,0 +1,93 @@
+<?php
+
+/* themes/custom/myboot/templates/node/node.html.twig */
+class __TwigTemplate_3e260bb9d108e20571d73eab56a2dfe0d11ffe3cb1893e0bab199bc7bebfc6df extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 1);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        $context["align"] = $this->getAttribute($this->getAttribute($this->getAttribute(($context["content"] ?? null), "field_options", array()), 0, array(), "array"), "#markup", array(), "array");
+        // line 2
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["align"] ?? null), "html", null, true));
+        echo "
+<div";
+        // line 3
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["attributes"] ?? null), "addClass", array(0 => ($context["align"] ?? null)), "method"), "html", null, true));
+        echo ">
+";
+        // line 4
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content"] ?? null), "field_textfield", array()), "html", null, true));
+        echo "
+</div>
+";
+        // line 6
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content"] ?? null), "field_options", array()), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/myboot/templates/node/node.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 6,  53 => 4,  49 => 3,  45 => 2,  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% set align = content.field_options[0]['#markup'] %}
+{{ align }}
+<div{{ attributes.addClass(align)}}>
+{{content.field_textfield}}
+</div>
+{{content.field_options}}", "themes/custom/myboot/templates/node/node.html.twig", "/Applications/MAMP/htdocs/drupal/FrontEnd/themes/custom/myboot/templates/node/node.html.twig");
+    }
+}
