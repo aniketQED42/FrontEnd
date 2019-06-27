0ba6df60f59b8512a4a1b85f7360991fd63dc2d2
aniketQED42 <aniket.gaikwad@QED42.com>
2019-06-27 19:28:10 +0530

    Worked on rounding off the temperaure values

--- modules/custom/weather/src/Plugin/Block/WeatherBlock.php
+++ modules/custom/weather/src/Plugin/Block/WeatherBlock.php
@@ -39,7 +39,18 @@ class WeatherBlock extends BlockBase {
         '#upload_location' => 'public://upload/myimage',
         '#title' => $this->t('Upload Image:'),
 
-    ];    
+    ];
+
+    $options = array(
+      'yes' => t('Yes'),
+      'no' => t('No'), 
+    );
+
+    $form['round'] = array(
+      '#title' => t('Please tick for round off values :'),
+      '#type' => 'checkboxes',
+      '#options' => $options,
+    );
     return $form;
   }
 
@@ -47,7 +58,8 @@ class WeatherBlock extends BlockBase {
     
     $this->setConfigurationValue('city',$form_state->getValue('city'));
     $this->setConfigurationValue('description',$form_state->getValue('description'));
-    $this->setConfigurationValue('image',$form_state->getValue('image'));       
+    $this->setConfigurationValue('image',$form_state->getValue('image'));
+    $this->setConfigurationValue('round',$form_state->getValue('round'));       
   }
 
   public function build() {
@@ -60,8 +72,31 @@ class WeatherBlock extends BlockBase {
     $image = ($config['image']);
     $img = \Drupal\file\Entity\File::load($image[0]);
     $imgpath = $img->getFileUri();
-
-    return [
+    $mintmp = $result['main']['temp_min'];
+    $maxtmp = $result['main']['temp_max'];
+    $pressure = $result['main']['pressure'];
+    $humidity = $result['main']['humidity'];
+    $temp = $result['main']['temp'];
+    if ($config['round']=='yes'){
+      $roundedmintmp = round($mintmp);
+      $roundedmaxtmp = round($maxtmp);
+      $roundedpressure = round($pressure);
+      $roundedhumidity = round($humidity);
+      $roundedtemp = round($temp);
+      return [   
+        
+        '#theme' => 'weather',
+        '#image' => $imgpath,
+        '#city' => $city,
+        '#description' => $description,
+        '#mintemp' => $roundedmintmp,
+        '#maxtemp' => $roundedmaxtmp,
+        '#pressure' => $roundedpressure,
+        '#humidity' => $roundedhumidity,
+        '#temp' => $roundedtemp,
+    ];
+  }    
+  return [    
         '#theme' => 'weather',
         '#image' => $imgpath,
         '#city' => $city,
@@ -71,7 +106,7 @@ class WeatherBlock extends BlockBase {
         '#pressure' => $result['main']['pressure'],
         '#humidity' => $result['main']['humidity'],
         '#temp' => $result['main']['temp']
-    ]; 
+    ];
     }
 
 }
